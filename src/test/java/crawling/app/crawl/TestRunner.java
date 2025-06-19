c4fba70d43f1eea79c14cbf73b9aa8b3c492004a
Moh Aarif Ahmed <arifahmed0091@gmail.com>
2025-06-19 18:38:51 +0530

    added logic for logging error

--- src/test/java/crawling/app/crawl/TestRunner.java
+++ src/test/java/crawling/app/crawl/TestRunner.java
@@ -25,83 +25,86 @@ import org.testng.annotations.Test;
 import utils.csvReader;
 
 public class TestRunner {
-	
+
 	WebDriver driver;
 	private Logger log = LogManager.getLogger(TestRunner.class);
 	@BeforeMethod
 	public void settingUp() {
-		
+
 	}
-	
+
 	@Test(dataProvider="csvReader2", dataProviderClass = csvReader.class)
 	public void crawl(Object[] data) {
 		int crawling = 0;
 		String store = null;
-		
-		while(crawling<data.length) {
-			String action = data[crawling].toString().split("=>")[0];
-			if(action.equals("open")) {
-				log.info("Initializing browser");
-				driver = new ChromeDriver();
-				String url = data[crawling].toString().split("=>")[1];
-				driver.get(url);
-				driver.manage().window().maximize();
-				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-			}else if(action.equals("click")) {
-				String param = data[crawling].toString().split("=>")[1];
-				log.info("performing click operation");
-				driver.findElement(By.xpath(param)).click();
-			}else if(action.equals("enterText")) {
-				String xpath = data[crawling].toString().split("=>")[1];
-				String text = data[crawling].toString().split("=>")[2];
-				driver.findElement(By.xpath(xpath)).sendKeys(text);
-			}else if(action.equals("jsClick")) {
-				System.out.println("invoked jsClick");
-				JavascriptExecutor js = (JavascriptExecutor) driver;
-				String xpath = data[crawling].toString().split("=>")[1];
-				WebElement obj = driver.findElement(By.xpath(xpath));
-				js.executeScript("arguments[0].click();", obj);
-			}else if(action.equals("selectFromStaticDropdown")) {
-				String xpath = data[crawling].toString().split("=>")[1];
-				String dataTobeSelected = data[crawling].toString().split("=>")[2];
-				Select select = new Select(driver.findElement(By.xpath(xpath)));
-				select.selectByVisibleText(dataTobeSelected);
-			} else if(action.equals("getText")) {
-				String xpath = data[crawling].toString().split("=>")[1];
-				String text = driver.findElement(By.xpath(xpath)).getText();
-				System.out.println(text);
-			}else if(action.equals("storeResult")) {
-				String xpath = data[crawling].toString().split("=>")[1];
-				store = driver.findElement(By.xpath(xpath)).getText();
-			}
-			else if(action.equals("assertEquals")) {
-				String expected = data[crawling].toString().split("=>")[1];
-				if(store !=null) {
-					Assert.assertEquals(store, expected, "Actual and expected condition not matched. Actual: "+store);
-				}else {
-					Assert.assertNotNull(store, "Cannot perform assert operation because no actual result is stored. please perform getText action first to store result");
-				}
-			}else if(action.equals("waitFor")) {
-				String xpath = data[crawling].toString().split("=>")[1];
-				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
-				
-			}else if(action.equals("getTexts")) {
-				System.out.println("invoking getTexts");
-				String xpath = data[crawling].toString().split("=>")[1];
-				WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
-				wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
-				List<String> texts = driver.findElements(By.xpath(xpath)).stream().map(e->e.getText()).collect(Collectors.toList());
-				for(String text: texts) {
+		try {
+			while(crawling<data.length) {
+				String action = data[crawling].toString().split("=>")[0];
+				if(action.equals("open")) {
+					log.info("Initializing browser");
+					driver = new ChromeDriver();
+					String url = data[crawling].toString().split("=>")[1];
+					driver.get(url);
+					driver.manage().window().maximize();
+					driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+				}else if(action.equals("click")) {
+					String param = data[crawling].toString().split("=>")[1];
+					log.info("performing click operation");
+					driver.findElement(By.xpath(param)).click();
+				}else if(action.equals("enterText")) {
+					String xpath = data[crawling].toString().split("=>")[1];
+					String text = data[crawling].toString().split("=>")[2];
+					driver.findElement(By.xpath(xpath)).sendKeys(text);
+				}else if(action.equals("jsClick")) {
+					System.out.println("invoked jsClick");
+					JavascriptExecutor js = (JavascriptExecutor) driver;
+					String xpath = data[crawling].toString().split("=>")[1];
+					WebElement obj = driver.findElement(By.xpath(xpath));
+					js.executeScript("arguments[0].click();", obj);
+				}else if(action.equals("selectFromStaticDropdown")) {
+					String xpath = data[crawling].toString().split("=>")[1];
+					String dataTobeSelected = data[crawling].toString().split("=>")[2];
+					Select select = new Select(driver.findElement(By.xpath(xpath)));
+					select.selectByVisibleText(dataTobeSelected);
+				} else if(action.equals("getText")) {
+					String xpath = data[crawling].toString().split("=>")[1];
+					String text = driver.findElement(By.xpath(xpath)).getText();
 					System.out.println(text);
+				}else if(action.equals("storeResult")) {
+					String xpath = data[crawling].toString().split("=>")[1];
+					store = driver.findElement(By.xpath(xpath)).getText();
 				}
+				else if(action.equals("assertEquals")) {
+					String expected = data[crawling].toString().split("=>")[1];
+					if(store !=null) {
+						Assert.assertEquals(store, expected, "Actual and expected condition not matched. Actual: "+store);
+					}else {
+						Assert.assertNotNull(store, "Cannot perform assert operation because no actual result is stored. please perform getText action first to store result");
+					}
+				}else if(action.equals("waitFor")) {
+					String xpath = data[crawling].toString().split("=>")[1];
+					WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+					wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+
+				}else if(action.equals("getTexts")) {
+					System.out.println("invoking getTexts");
+					String xpath = data[crawling].toString().split("=>")[1];
+					WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+					wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+					List<String> texts = driver.findElements(By.xpath(xpath)).stream().map(e->e.getText()).collect(Collectors.toList());
+					for(String text: texts) {
+						System.out.println(text);
+					}
+				}
+
+
+
+				crawling +=1;
 			}
-			
-			
-			
-			crawling +=1;
+		}catch(Exception e) {
+			log.error(e.getMessage());
 		}
-		
+
 	}
 	////div[@id='showApplication0']//div[contains(@class,'step')]//div[@name='statusName']/span
 	//,jsClick=>//div[contains(@class, 'owl-item')][1]//div[contains(@class,'customCard3')]//button[contains(@class,'customButton')]
